7fe69981771f6c6cf9213bb1ee28b8917de1a652
tahataha005 <taha.f.taha0@gmail.com>
2022-09-23 22:30:26 +0300

    Editing loading news script

--- frontend/javascript/script.js
+++ frontend/javascript/script.js
@@ -1,26 +1,22 @@
 $(document).ready(function(){
 
         $.getJSON("http://localhost/news-website/backend/get_new.php",function(data){
-            console.log(data)
+            for(let i =0;i<data.length;i++){
+              console.log(i)
+              $("#newsContainer").append(`<div class="card">
+              <div class="card-header">
+                ${data[i].title}
+              </div>
+              <div class="card-body">
+                <p class="card-text">${data[i].content}</p>
+              </div>
+              <div class="card-footer text-muted d-flex bg-dark justify-content-center align-items-center">
+                <p class="text-primary">${data[i].time}</p>
+              </div>
+            </div>`)
+          }
         })
     })
-            for(let i =0;i<data.length;i++){
-                console.log(i)
-                $("#newsContainer").append(`<div class="card">
-                <div class="card-header">
-                  ${data[i].title}
-                </div>
-                <div class="card-body">
-                  <p class="card-text">${data[i].content}</p>
-                </div>
-                <div class="card-footer text-muted d-flex bg-dark justify-content-center align-items-center">
-                  <p class="text-primary">${data[i].time}</p>
-                </div>
-              </div>`)
-            }
-                
+            
 
-            });
-
-        })
    
